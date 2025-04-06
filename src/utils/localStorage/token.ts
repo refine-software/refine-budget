3493161e532f72add92e46f2c34b6d46d3ad3acd
sharon <ali93456@gmail.com>
2025-04-06 19:28:37 +0300

    feat: enhance access token management with expiration handling

--- src/utils/localStorage/token.ts
+++ src/utils/localStorage/token.ts
@@ -1,3 +1,25 @@
-export function getAccessToken(): string | null {
-  return localStorage.getItem("accessToken");
+import { dateConversion } from "../date";
+
+type AccessToken = {
+  accessToken: string;
+  accessTokenExp: Date;
+}
+
+export function getAccessToken(): AccessToken | null {
+  const token = localStorage.getItem("accessToken");
+  const tokenExp = localStorage.getItem("accessTokenExp");
+  if (!token || !tokenExp) return null;
+
+  const date = dateConversion(tokenExp);
+  if (date === null) return null;
+
+  return { accessToken: token, accessTokenExp: date };
+}
+
+export function setAccessToken(accessToken: AccessToken): void {
+  const { accessToken: token, accessTokenExp } = accessToken;
+  const exp = accessTokenExp.toLocaleString();
+  localStorage.setItem("accessToken", token);
+  localStorage.setItem("accessTokenExp", exp);
+
 }
