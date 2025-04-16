5f25cd04c737b3442c04529d526842efaaab6c9d
sharon <ali93456@gmail.com>
2025-04-16 18:00:49 +0300

    refactor: integrate authentication context and navigation in Register component

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,16 @@
+import { useContext, useEffect } from "react";
 import AuthNav from "../components/navbar/AuthNav";
+import { AuthContext } from "../store/auth-context";
+import { useNavigate } from "react-router";
 
 const Register = () => {
+    const auth = useContext(AuthContext);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (auth.authenticated)
+            navigate("/");
+    }, [auth, navigate]);
+
     return (
         <>
             <AuthNav />
