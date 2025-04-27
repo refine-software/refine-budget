2610f6e777cfb2244aa9ede9d645a2303e91c748
sharon <ali93456@gmail.com>
2025-04-27 18:01:13 +0300

    feat: implement Cards component to display transaction history

---
+++ src/components/history/Cards.tsx
@@ -0,0 +1,84 @@
+import { useState, useRef, useEffect } from "react";
+import { Transaction, TransactionTypes } from "../../types";
+import { strftime } from "@sharon-xa/strftime";
+
+const Card = ({ transaction }: { transaction: Transaction }) => {
+    const [expanded, setExpanded] = useState(false);
+    const [showReadMore, setShowReadMore] = useState(false);
+    const [shortText, setShortText] = useState("");
+    const descriptionRef = useRef<HTMLDivElement>(null);
+
+    const isDeposit = transaction.transaction_type === TransactionTypes.deposit;
+    const [formattedDate, err] = strftime(new Date(transaction.transaction_date), "b d, Y");
+
+    const maxChars = 80;
+    useEffect(() => {
+        if (transaction.description.length > maxChars) {
+            setShowReadMore(true);
+            setShortText(transaction.description.slice(0, maxChars));
+        } else {
+            setShortText(transaction.description);
+            setShowReadMore(false);
+        }
+    }, [transaction.description]);
+
+
+    return (
+        <div className={`min-h-52 bg-card-bg rounded-3xl my-5 p-5 flex flex-col gap-6 shadow-xl transition-all duration-300`} key={transaction.id}>
+            <div className={`capitalize text-2xl font-bold ${isDeposit ? "text-green" : "text-red"}`}>
+                {transaction.transaction_type}
+            </div>
+
+            <div
+                ref={descriptionRef}
+                className="font-medium text-white-50 overflow-hidden"
+            >
+                {!expanded ? (
+                    <>
+                        {shortText}
+                        {showReadMore ? "..." : ""}
+                        {showReadMore && (
+                            <button
+                                className="text-primary font-semibold inline ml-2 underline"
+                                onClick={() => setExpanded(true)}
+                            >
+                                More
+                            </button>
+                        )}
+                    </>
+                ) : (
+                    <>
+                        {transaction.description}
+                        <button
+                            className="text-primary font-semibold inline ml-2 underline"
+                            onClick={() => setExpanded(false)}
+                        >
+                            Less
+                        </button>
+                    </>
+                )}
+            </div>
+
+            <div className="flex justify-between mt-auto">
+                <div className='text-xl font-medium text-white-75'>
+                    {err !== null ? "couldn't parse date" : formattedDate}
+                </div>
+                <div className={`text-xl font-medium ${isDeposit ? "text-green" : "text-red"}`}>
+                    {isDeposit ? "+" : "-"}{transaction.amount} IQD
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Cards = ({ transactions }: { transactions: Transaction[] }) => {
+    return (
+        <>
+            {transactions.map(transaction => (
+                <Card key={transaction.id} transaction={transaction} />
+            ))}
+        </>
+    );
+};
+
+export default Cards;
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,8 +1,37 @@
+import { JSX, useState } from "react";
+import { getTransactions } from "../api/transactions";
+import { useQuery } from "@tanstack/react-query";
+import { SortTypes, TransactionsReqQueries } from "../types";
+import Cards from "../components/history/Cards";
 
 const History = () => {
+    const [transReqQuery, setTransReqQuery] = useState<TransactionsReqQueries>({
+        page: 1,
+        limit: 10,
+        sort: SortTypes.dateDesc,
+    });
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ["transactions"],
+        queryFn: getTransactions.bind(transReqQuery),
+    });
+
+    let body: string | JSX.Element = "";
+
+    if (isPending) {
+        body = `<span>Loading...</span>`
+    }
+
+    if (isError) {
+        body = `<span>Error: ${error.message}</span>`
+    }
+
+    if (!isError && !isPending && data) {
+        body = <Cards transactions={data.transactions} />
+    }
+
     return (
-        <div>
-            HIstory
+        <div className="mb-20">
+            {body}
         </div>
     )
 }
