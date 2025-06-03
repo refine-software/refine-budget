b1a11bbbcee163c8df62acee2f78b9017884fcbe
mustafa <ma9905918@gmail.com>
2025-06-03 23:54:19 +0300

    better date handling for token date expiration

--- src/utils/localStorage/token.ts
+++ src/utils/localStorage/token.ts
@@ -1,24 +1,31 @@
 import { dateConversion } from "../date";
 
 type AccessToken = {
-  accessToken: string;
-  accessTokenExp: Date;
-}
+	accessToken: string;
+	accessTokenExp: Date;
+};
 
 export function getAccessToken(): AccessToken | null {
-  const token = localStorage.getItem("accessToken");
-  const tokenExp = localStorage.getItem("accessTokenExp");
-  if (!token || !tokenExp) return null;
+	const token = localStorage.getItem("accessToken");
+	const tokenExp = localStorage.getItem("accessTokenExp");
+	if (!token || !tokenExp) return null;
 
-  const date = dateConversion(tokenExp);
-  if (date === null) return null;
+	const date = dateConversion(tokenExp);
+	if (date === null) {
+		console.log("bad date");
+		return null;
+	}
 
-  return { accessToken: token, accessTokenExp: date };
+	return { accessToken: token, accessTokenExp: date };
 }
 
 export function setAccessToken(accessToken: AccessToken): void {
-  const { accessToken: token, accessTokenExp } = accessToken;
-  const exp = accessTokenExp.toLocaleString();
-  localStorage.setItem("accessToken", token);
-  localStorage.setItem("accessTokenExp", exp);
+	const { accessToken: token, accessTokenExp } = accessToken;
+	const exp = accessTokenExp.toLocaleString();
+	localStorage.setItem("accessToken", token);
+	localStorage.setItem("accessTokenExp", exp);
+}
+export function clearAccessToken(): void {
+	localStorage.removeItem("accessToken");
+	localStorage.removeItem("accessTokenExp");
 }
