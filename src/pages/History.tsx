84414edcb6a072d64b5b3d84cb495d8dc93eff08
mustafa <ma9905918@gmail.com>
2025-06-03 23:58:24 +0300

    refactor: streamline transaction fetching and loading state handling in History component

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,39 +1,30 @@
-import { JSX, useState } from "react";
-import { getTransactions } from "../api/transactions";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { getTransactions } from "../api/transactions";
 import { SortTypes, TransactionsReqQueries } from "../types";
 import Cards from "../components/history/Cards";
 
 const History = () => {
-    const [transReqQuery, setTransReqQuery] = useState<TransactionsReqQueries>({
-        page: 1,
-        limit: 10,
-        sort: SortTypes.dateDesc,
-    });
-    const { isPending, isError, data, error } = useQuery({
-        queryKey: ["transactions"],
-        queryFn: getTransactions.bind(transReqQuery),
-    });
+	const [transReqQuery, setTransReqQuery] = useState<TransactionsReqQueries>({
+		page: 1,
+		limit: 10,
+		sort: SortTypes.dateDesc,
+	});
 
-    let body: string | JSX.Element = "";
+	const { isPending, isError, data, error } = useQuery({
+		queryKey: ["transactions", transReqQuery],
+		queryFn: () => getTransactions.call(transReqQuery),
+	});
+	let body;
 
-    if (isPending) {
-        body = `<span>Loading...</span>`
-    }
-
-    if (isError) {
-        body = `<span>Error: ${error.message}</span>`
-    }
-
-    if (!isError && !isPending && data) {
-        body = <Cards transactions={data.transactions} />
-    }
-
-    return (
-        <div className="mb-20">
-            {body}
-        </div>
-    )
-}
+	if (isPending) {
+		body = <span>Loading...</span>;
+	} else if (isError) {
+		body = <span>Error: {(error as Error).message}</span>;
+	} else if (data) {
+		body = <Cards transactions={data.transactions} />;
+	}
+	return <div className="mb-20">{body}</div>;
+};
 
 export default History;
