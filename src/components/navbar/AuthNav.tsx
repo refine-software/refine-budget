fd87f5eb51c0239e94954162329dc06a5c796ce3
mustafa <ma9905918@gmail.com>
2025-06-03 23:57:55 +0300

    style: consistency in AuthNav and Navbar components

--- src/components/navbar/AuthNav.tsx
+++ src/components/navbar/AuthNav.tsx
@@ -1,21 +1,31 @@
 import { NavLink } from "react-router";
 
 const AuthNav = () => {
-    return (
-        <div className="flex justify-center items-center gap-8">
-            <NavLink to="/login" className={({ isActive }) =>
-                `${isActive ? "bg-primary" : ""} block border-1 border-primary w-32 text-center px-4 py-2 rounded-md`
-            }>
-                Login
-            </NavLink>
+	return (
+		<div className="flex justify-center items-center gap-8">
+			<NavLink
+				to="/login"
+				className={({ isActive }) =>
+					`${
+						isActive ? "bg-primary" : ""
+					} block border-1 border-primary w-32 text-center px-4 py-2 `
+				}
+			>
+				Login
+			</NavLink>
 
-            <NavLink to="/register" className={({ isActive }) =>
-                `${isActive ? "bg-primary" : ""} block border-1 border-primary w-32 text-center px-4 py-2 rounded-md`
-            }>
-                Register
-            </NavLink>
-        </div>
-    );
-}
+			<NavLink
+				to="/register"
+				className={({ isActive }) =>
+					`${
+						isActive ? "bg-primary" : ""
+					} block border-1 border-primary w-32 text-center px-4 py-2 `
+				}
+			>
+				Register
+			</NavLink>
+		</div>
+	);
+};
 
 export default AuthNav;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,51 +4,65 @@ import { AuthContext } from "../../store/auth-context";
 import { Role } from "../../types";
 
 const Navbar = () => {
-    const { role } = useContext(AuthContext);
-    const isAdmin = role === Role.admin;
+	const { role } = useContext(AuthContext);
+	const isAdmin = role === Role.admin;
 
-    return (
-        <nav className="bg-primary fixed w-full overflow-hidden bottom-0 z-50 rounded-t-4xl">
-            <div className="py-5 px-8 flex justify-around gap-4">
-                <NavLink to={"/"}>
-                    {({ isActive }) => (
-                        <img
-                            src={isActive ? "./public/home-active.svg" : "/public/home.svg"}
-                            alt="Home"
-                        />
-                    )}
-                </NavLink>
-                <NavLink to={"/history"}>
-                    {({ isActive }) => (
-                        <img
-                            src={isActive ? "/public/history-active.svg" : "/public/history.svg"}
-                            alt="history"
-                        />
-                    )}
-                </NavLink>
-                <NavLink to={"/profile"}>
-                    {({ isActive }) => (
-                        <img
-                            src={isActive ? "/public/profile-active.svg" : "/public/profile.svg"}
-                            alt="Home"
-                        />
-                    )}
-                </NavLink>
-                {
-                    isAdmin && (
-                        <NavLink to={"/control"}>
-                            {({ isActive }) => (
-                                <img
-                                    src={isActive ? "/public/control-active.svg" : "/public/control.svg"}
-                                    alt="Home"
-                                />
-                            )}
-                        </NavLink>
-                    )
-                }
-            </div>
-        </nav >
-    );
+	return (
+		<nav className="bg-primary fixed w-full overflow-hidden bottom-0 z-50 ">
+			<div className="py-5 px-8 flex justify-around gap-4">
+				<NavLink to={"/"}>
+					{({ isActive }) => (
+						<img
+							src={
+								isActive
+									? "./public/home-active.svg"
+									: "/public/home.svg"
+							}
+							alt="Home"
+						/>
+					)}
+				</NavLink>
+				<NavLink to={"/history"}>
+					{({ isActive }) => (
+						<img
+							src={
+								isActive
+									? "/public/history-active.svg"
+									: "/public/history.svg"
+							}
+							alt="history"
+						/>
+					)}
+				</NavLink>
+				<NavLink to={"/profile"}>
+					{({ isActive }) => (
+						<img
+							src={
+								isActive
+									? "/public/profile-active.svg"
+									: "/public/profile.svg"
+							}
+							alt="Home"
+						/>
+					)}
+				</NavLink>
+				{isAdmin && (
+					<NavLink to={"/control"}>
+						{({ isActive }) => (
+							<img
+								src={
+									isActive
+										? "/public/control-active.svg"
+										: "/public/control.svg"
+								}
+								alt="Home"
+							/>
+						)}
+					</NavLink>
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
