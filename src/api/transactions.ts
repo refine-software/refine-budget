096461c4ed519f49a8619ad886205b3b417c9989
mustafa <ma9905918@gmail.com>
2025-06-04 03:06:38 +0300

    refactor: enhance transaction fetching with improved error handling and authorization token format

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -1,34 +1,51 @@
 import axios, { AxiosError } from "axios";
 import { getAccessToken } from "../utils";
 import api from "./axiosConfig";
-import { TransactionsReqQueries, TransactionsRes, TransactionTypes } from "../types";
+import {
+	TransactionsReqQueries,
+	TransactionsRes,
+	TransactionTypes,
+} from "../types";
 
-export async function getTransactions(this: TransactionsReqQueries): Promise<TransactionsRes> {
-    const accTokenObj = getAccessToken();
-    if (accTokenObj === null)
-        throw new Error("you're not authorized");
+export async function getTransactions(
+	this: TransactionsReqQueries
+): Promise<TransactionsRes> {
+	const accTokenObj = getAccessToken();
+	if (!accTokenObj) throw new Error("You're not authorized");
+	const queries: string[] = [];
 
-    const queries: string[] = [];
-    if (this.page) queries.push("page=" + this.page);
-    if (this.limit) queries.push("limit=" + this.limit);
-    if (this.sort) queries.push("sort=" + this.sort);
-    if (this.transactionType) queries.push("trans-type=" + this.transactionType);
-    if (this.depositTypes && this.transactionType !== TransactionTypes.withdrawal && this.depositTypes.length > 0)
-        this.depositTypes.forEach((depType) => queries.push("deposit-type=" + depType))
+	if (this.page) queries.push("page=" + this.page);
+	if (this.limit) queries.push("limit=" + this.limit);
+	if (this.sort) queries.push("sort=" + this.sort);
+	if (this.transactionType)
+		queries.push("trans-type=" + this.transactionType);
 
-    try {
-        const res = await api.get("/transaction/history?" + queries.join("&"),
-            {
-                headers: {
-                    Authorization: accTokenObj.accessToken,
-                },
-            }
-        );
-        return res.data as TransactionsRes;
-    } catch (err) {
-        if (axios.isAxiosError(err))
-            throw err as AxiosError;
-        else
-            throw err as Error;
-    }
+	if (
+		this.depositTypes &&
+		this.transactionType !== TransactionTypes.withdrawal &&
+		this.depositTypes.length > 0
+	) {
+		this.depositTypes.forEach((depType) =>
+			queries.push("deposit-type=" + depType)
+		);
+	}
+
+	try {
+		const res = await api.get("/transaction/history?" + queries.join("&"), {
+			headers: {
+				Authorization: `Bearer ${accTokenObj.accessToken}`,
+			},
+		});
+
+		if (!res.data || typeof res.data !== "object") {
+			throw new Error("there are no transactions yet");
+		}
+		return res.data as TransactionsRes;
+	} catch (err) {
+		if (axios.isAxiosError(err)) {
+			throw err as AxiosError;
+		} else {
+			throw err as Error;
+		}
+	}
 }
