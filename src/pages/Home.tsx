b031c49680295c9f75a29c17b0086def05788119
sharon <ali93456@gmail.com>
2025-07-07 09:05:59 +0300

    style: adjust margin and structure for Home component for improved layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,17 @@ import DebtMoney from "../components/home/DebtMoney";
 
 const Home = () => {
 	return (
-		<div className="h-full mt-8 flex flex-col justify-center items-center gap-12 text-center">
+		<div className="h-full mt-4 flex flex-col justify-center items-center gap-12 text-center">
 			<p className="text-primary text-4xl font-bold">Team Budget</p>
-			<div className="py-10 rounded-2xl w-full flex flex-col justify-center items-center gap-6 bg-grey shadow-2xl">
-				<p className="text-3xl font-bold">Our Budget</p>
-				<BudgetMoney func={getBudget} />
-			</div>
-			<div className="py-10 rounded-2xl w-full flex flex-col justify-center items-center gap-6 bg-grey shadow-2xl">
-				<p className="text-3xl font-bold">Your Debt</p>
-				<DebtMoney />
+			<div className="w-full flex flex-col justify-center items-center gap-6">
+				<div className="py-10 rounded-2xl w-full flex flex-col justify-center items-center gap-6 bg-grey shadow-2xl">
+					<p className="text-3xl font-bold">Our Budget</p>
+					<BudgetMoney func={getBudget} />
+				</div>
+				<div className="py-10 rounded-2xl w-full flex flex-col justify-center items-center gap-6 bg-grey shadow-2xl">
+					<p className="text-3xl font-bold">Your Debt</p>
+					<DebtMoney />
+				</div>
 			</div>
 		</div>
 	);
